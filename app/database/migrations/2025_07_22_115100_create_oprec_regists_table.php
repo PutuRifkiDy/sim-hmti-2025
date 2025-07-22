95503c3d11d108809382099ca979f4aa404d514c
PutuRifkiDy <puturifki56@gmail.com>
2025-07-22 19:53:58 +0800

    added: Model and Migration Oprec Regists

---
+++ app/database/migrations/2025_07_22_115100_create_oprec_regists_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('oprec_regists', function (Blueprint $table) {
+            $table->id();
+            $table->string('reason_join');
+            $table->string('experience');
+            $table->foreignId('sie_id')->constrained('master_sies')->cascadeOnDelete();
+            $table->foreignId('oprec_id')->constrained('oprecs')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('oprec_regists');
+    }
+};
