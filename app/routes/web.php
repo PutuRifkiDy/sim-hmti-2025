a66ec1b737563a04e82eb695500da7b8b513cb21
PutuRifkiDy <puturifki56@gmail.com>
2025-08-07 19:34:27 +0800

    update: routes and middleware

--- app/routes/web.php
+++ app/routes/web.php
@@ -30,95 +30,102 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::controller(MasterUserController::class)->group(function () {
-    Route::get('/master-user', 'index')->name('master-user.index');
-    Route::get('/master-user/create', 'create')->name('master-user.create');
-    Route::post('/master-user/store', 'store')->name('master-user.store');
-    Route::get('/master-user/{id}/edit', 'edit')->name('master-user.edit');
-    Route::put('/master-user/{id}/update', 'update')->name('master-user.update');
-    Route::delete('/master-user/{id}/delete', 'destroy')->name('master-user.destroy');
+    Route::get('/dashboard/admin/master-user', 'index')->name('master-user.index');
+    Route::get('/dashboard/admin/master-user/create', 'create')->name('master-user.create');
+    Route::post('/dashboard/admin/master-user/store', 'store')->name('master-user.store');
+    Route::get('/dashboard/admin/master-user/{id}/edit', 'edit')->name('master-user.edit');
+    Route::put('/dashboard/admin/master-user/{id}/update', 'update')->name('master-user.update');
+    Route::delete('/dashboard/admin/master-user/{id}/delete', 'destroy')->name('master-user.destroy');
 })->middleware(['auth', 'role:divisi_it']);
 
 Route::controller(MasterSieController::class)->group(function () {
-    Route::get('/master-sie', 'index')->name('master-sie.index');
-    Route::get('/master-sie/create', 'create')->name('master-sie.create');
-    Route::post('/master-sie/store', 'store')->name('master-sie.store');
-    Route::get('/master-sie/{id}/edit', 'edit')->name('master-sie.edit');
-    Route::put('/master-sie/{id}/update', 'update')->name('master-sie.update');
-    Route::delete('/master-sie/{id}/delete', 'destroy')->name('master-sie.destroy');
+    Route::get('/dashboard/admin/master-sie', 'index')->name('master-sie.index');
+    Route::get('/dashboard/admin/master-sie/create', 'create')->name('master-sie.create');
+    Route::post('/dashboard/admin/master-sie/store', 'store')->name('master-sie.store');
+    Route::get('/dashboard/admin/master-sie/{id}/edit', 'edit')->name('master-sie.edit');
+    Route::put('/dashboard/admin/master-sie/{id}/update', 'update')->name('master-sie.update');
+    Route::delete('/dashboard/admin/master-sie/{id}/delete', 'destroy')->name('master-sie.destroy');
 })->middleware(['auth', 'role:divisi_it']);
 
 Route::controller(MasterPeriodController::class)->group(function () {
-    Route::get('/master-period', 'index')->name('master-period.index');
-    Route::get('/master-period/create', 'create')->name('master-period.create');
-    Route::post('/master-period/store', 'store')->name('master-period.store');
-    Route::get('/master-period/{id}/edit', 'edit')->name('master-period.edit');
-    Route::put('/master-period/{id}/update', 'update')->name('master-period.update');
-    Route::delete('/master-period/{id}/delete', 'destroy')->name('master-period.destroy');
+    Route::get('/dashboard/admin/master-period', 'index')->name('master-period.index');
+    Route::get('/dashboard/admin/master-period/create', 'create')->name('master-period.create');
+    Route::post('/dashboard/admin/master-period/store', 'store')->name('master-period.store');
+    Route::get('/dashboard/admin/master-period/{id}/edit', 'edit')->name('master-period.edit');
+    Route::put('/dashboard/admin/master-period/{id}/update', 'update')->name('master-period.update');
+    Route::delete('/dashboard/admin/master-period/{id}/delete', 'destroy')->name('master-period.destroy');
 })->middleware(['auth', 'role:divisi_it']);
 
 Route::controller(MasterPositionController::class)->group(function () {
-    Route::get('/master-position', 'index')->name('master-position.index');
-    Route::get('/master-position/create', 'create')->name('master-position.create');
-    Route::post('/master-position/store', 'store')->name('master-position.store');
-    Route::get('/master-position/{id}/edit', 'edit')->name('master-position.edit');
-    Route::put('/master-position/{id}/update', 'update')->name('master-position.update');
-    Route::delete('/master-position/{id}/delete', 'destroy')->name('master-position.destroy');
+    Route::get('/dashboard/admin/master-position', 'index')->name('master-position.index');
+    Route::get('/dashboard/admin/master-position/create', 'create')->name('master-position.create');
+    Route::post('/dashboard/admin/master-position/store', 'store')->name('master-position.store');
+    Route::get('/dashboard/admin/master-position/{id}/edit', 'edit')->name('master-position.edit');
+    Route::put('/dashboard/admin/master-position/{id}/update', 'update')->name('master-position.update');
+    Route::delete('/dashboard/admin/master-position/{id}/delete', 'destroy')->name('master-position.destroy');
 })->middleware(['auth', 'role:divisi_it']);
 
 Route::controller(MasterHimpunanController::class)->group(function () {
-    Route::get('/master-period/master-himpunan/{id}/show', 'index')->name('master-himpunan.index');
-    Route::get('/master-period/master-himpunan/create/{id}', 'create')->name('master-himpunan.create');
-    Route::post('/master-period/master-himpunan/store/{id}', 'store')->name('master-himpunan.store');
-    Route::get('/master-period/master-himpunan/{id}/edit', 'edit')->name('master-himpunan.edit');
-    Route::put('/master-period/master-himpunan/{id}/update', 'update')->name('master-himpunan.update');
-    Route::delete('/master-period/master-himpunan/{id}/delete', 'destroy')->name('master-himpunan.destroy');
+    Route::get('/dashboard/admin/master-period/master-himpunan/{id}/show', 'index')->name('master-himpunan.index');
+    Route::get('/dashboard/admin/master-period/master-himpunan/create/{id}', 'create')->name('master-himpunan.create');
+    Route::post('/dashboard/admin/master-period/master-himpunan/store/{id}', 'store')->name('master-himpunan.store');
+    Route::get('/dashboard/admin/master-period/master-himpunan/{id}/edit', 'edit')->name('master-himpunan.edit');
+    Route::put('/dashboard/admin/master-period/master-himpunan/{id}/update', 'update')->name('master-himpunan.update');
+    Route::delete('/dashboard/admin/master-period/master-himpunan/{id}/delete', 'destroy')->name('master-himpunan.destroy');
 })->middleware(['auth', 'role:divisi_it']);
 
 Route::controller(MasterProgramKerjaController::class)->group(function () {
-    Route::get('/master-period/master-program-kerja/{id}/show', 'index')->name('master-program-kerja.index');
-    Route::get('/master-period/master-program-kerja/create/{id}', 'create')->name('master-program-kerja.create');
-    Route::post('/master-period/master-program-kerja/store/{id}', 'store')->name('master-program-kerja.store');
-    Route::get('/master-period/master-program-kerja/{id}/edit', 'edit')->name('master-program-kerja.edit');
-    Route::put('/master-period/master-program-kerja/{id}/update', 'update')->name('master-program-kerja.update');
-    Route::delete('/master-period/master-program-kerja/{id}/delete', 'destroy')->name('master-program-kerja.destroy');
+    Route::get('/dashboard/admin/master-period/master-program-kerja/{id}/show', 'index')->name('master-program-kerja.index');
+    Route::get('/dashboard/admin/master-period/master-program-kerja/create/{id}', 'create')->name('master-program-kerja.create');
+    Route::post('/dashboard/admin/master-period/master-program-kerja/store/{id}', 'store')->name('master-program-kerja.store');
+    Route::get('/dashboard/admin/master-period/master-program-kerja/{id}/edit', 'edit')->name('master-program-kerja.edit');
+    Route::put('/dashboard/admin/master-period/master-program-kerja/{id}/update', 'update')->name('master-program-kerja.update');
+    Route::delete('/dashboard/admin/master-period/master-program-kerja/{id}/delete', 'destroy')->name('master-program-kerja.destroy');
 })->middleware(['auth', 'role:divisi_it']);
 
 Route::controller(MasterOpenRekruitmen::class)->group(function () {
-    Route::get('/master-open-rekruitmen', 'index')->name('master-open-rekruitmen.index');
-    Route::get('/master-open-rekruitmen/create', 'create')->name('master-open-rekruitmen.create');
-    Route::post('/master-open-rekruitmen/store', 'store')->name('master-open-rekruitmen.store');
-    Route::get('/master-open-rekruitmen/{id}/edit', 'edit')->name('master-open-rekruitmen.edit');
-    Route::put('/master-open-rekruitmen/{id}/update', 'update')->name('master-open-rekruitmen.update');
-    Route::delete('/master-open-rekruitmen/{id}/delete', 'destroy')->name('master-open-rekruitmen.destroy');
-    Route::get('master-open-rekruitmen/{id}/see-registered', 'seeRegistered')->name('master-open-rekruitmen.see-registered');
-})->middleware(['auth', 'role:divisi_it,ketua_kegiatan']);
+    Route::get('/dashboard/admin/master-open-rekruitmen', 'index')->name('master-open-rekruitmen.index')
+        ->middleware(['auth', 'role:divisi_it|ketua_kegiatan']);
+    Route::get('/dashboard/admin/master-open-rekruitmen/create', 'create')->name('master-open-rekruitmen.create')
+        ->middleware(['auth', 'role:divisi_it']);
+    Route::post('/dashboard/admin/master-open-rekruitmen/store', 'store')->name('master-open-rekruitmen.store')
+        ->middleware(['auth', 'role:divisi_it']);
+    Route::get('/dashboard/admin/master-open-rekruitmen/{id}/edit', 'edit')->name('master-open-rekruitmen.edit')
+        ->middleware(['auth', 'role:divisi_it']);
+    Route::put('/dashboard/admin/master-open-rekruitmen/{id}/update', 'update')->name('master-open-rekruitmen.update')
+        ->middleware(['auth', 'role:divisi_it']);
+    Route::delete('/dashboard/admin/master-open-rekruitmen/{id}/delete', 'destroy')->name('master-open-rekruitmen.destroy')
+        ->middleware(['auth', 'role:divisi_it']);
+    Route::get('/dashboard/admin/master-open-rekruitmen/{id}/see-registered', 'seeRegistered')->name('master-open-rekruitmen.see-registered')
+        ->middleware(['auth', 'role:divisi_it|ketua_kegiatan']);
+});
 
 Route::controller(MasterFinancialController::class)->group(function () {
-    Route::get('/master-financial/{id}/show', 'index')->name('master-financial.index');
-    Route::get('/master-financial/create/{id}', 'create')->name('master-financial.create');
-    Route::post('/master-financial/store', 'store')->name('master-financial.store');
-    Route::get('/master-financial/{id}/edit', 'edit')->name('master-financial.edit');
-    Route::put('/master-financial/{id}/update', 'update')->name('master-financial.update');
-    Route::delete('/master-financial/{id}/delete', 'destroy')->name('master-financial.destroy');
+    Route::get('/dashboard/admin/master-financial/{id}/show', 'index')->name('master-financial.index');
+    Route::get('/dashboard/admin/master-financial/create/{id}', 'create')->name('master-financial.create');
+    Route::post('/dashboard/admin/master-financial/store', 'store')->name('master-financial.store');
+    Route::get('/dashboard/admin/master-financial/{id}/edit', 'edit')->name('master-financial.edit');
+    Route::put('/dashboard/admin/master-financial/{id}/update', 'update')->name('master-financial.update');
+    Route::delete('/dashboard/admin/master-financial/{id}/delete', 'destroy')->name('master-financial.destroy');
 })->middleware(['auth', 'role:divisi_it']);
 
 Route::controller(OprecRegistController::class)->group(function () {
-    Route::get('/master-open-rekruitmen/master-oprec-regist', 'index')->name('oprec-regist.index');
-    Route::get('/master-open-rekruitmen/master-oprec-regist/show/{id}', 'show')->name('oprec-regist.show');
+    Route::get('/master-oprec-regist', 'index')->name('oprec-regist.index');
+    Route::get('/master-oprec-regist/show/{id}', 'show')->name('oprec-regist.show');
     Route::post('/master-open-rekruitmen/master-oprec-regist/store', 'store')->name('oprec-regist.store');
     Route::get('/master-open-rekruitmen/master-oprec-regist/{idOprec}/registered/{idUser}', 'registered')->name('oprec-regist.edit');
-})->middleware('auth');
+})->middleware(['auth', 'role:guest|ketua_kegiatan|divisi_it']);
 
 Route::controller(ADARTController::class)->group(function () {
     Route::get('/master-adart', 'index')->name('master-adart.index');
-})->middleware('auth');
+})->middleware(['auth', 'role:guest|ketua_kegiatan|divisi_it']);
 
 Route::controller(MasterGrafikController::class)->group(function () {
-    Route::get('/master-grafik-keuangan', 'index')->name('master-grafik.index');
-    Route::get('/master-grafik-keuangan/show', 'show')->name('master-grafik.show');
+    Route::get('/dashboard/admin/master-grafik-keuangan', 'index')->name('master-grafik.index');
+    Route::get('/dashboard/admin/master-grafik-keuangan/show', 'show')->name('master-grafik.show');
 })->middleware(['auth', 'role:divisi_it']);
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'role:guest|ketua_kegiatan|divisi_it'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
