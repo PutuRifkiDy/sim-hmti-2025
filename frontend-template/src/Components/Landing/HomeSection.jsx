6325fdd339de87d8f4d10d5fa6bbf881f87b4a41
Najwa Tahir <najwatahir186@gmail.com>
2025-08-10 22:54:38 +0800

    style: add responsive mobile image

--- frontend-template/src/Components/Landing/HomeSection.jsx
+++ frontend-template/src/Components/Landing/HomeSection.jsx
@@ -1,43 +1,47 @@
 export default function HomeSection() {
   return (
     <div className="w-full bg-white">
-      <div className="flex flex-col md:flex-row items-center md:items-center h-full">
-        <div className="md:w-1/2 w-full flex flex-col md:flex-row items-center md:items-start p-8 md:p-12 space-x-0 md:space-x-4">
-          <div className="mb-4 md:mb-8 flex justify-center md:justify-start">
+      <div className="flex flex-col lg:flex-row items-center lg:items-center h-full">
+        <div className="lg:w-1/2 w-full flex flex-col h-full lg:flex-row items-center lg:items-start p-8 lg:p-12 space-x-0 lg:space-x-4">
+          <div className="mb-4 lg:mb-8 flex justify-center lg:justify-start">
             <img
               src="Logo-HMTI.png"
               alt="Logo HMTI"
-              className="w-32 md:w-72 h-auto"
+              className="w-32 lg:w-56 h-auto"
             />
           </div>
 
-          <div className="text-left flex flex-col items-center md:items-start w-[233px]">
+          <div className="text-left flex flex-col items-center lg:items-start w-[233px]">
             <div className="relative">
               <span
-                className="block text-5xl md:text-6xl lg:text-7xl font-normal text-[#785233] leading-none"
+                className="block text-5xl lg:text-7xl font-normal text-[#785233] leading-none"
                 style={{ fontFamily: "Arrintika Signature, cursive" }}
               >
                 Kabinet
               </span>
-              <h1 className="uppercase text-5xl md:text-6xl lg:text-7xl font-bold text-[#ECC067] leading-none -mt-2 md:-mt-4 font-poppins">
+              <h1 className="uppercase text-5xl lg:text-7xl font-bold text-[#ECC067] leading-none -mt-2 lg:-mt-4 font-poppins">
                 Harmoni
               </h1>
             </div>
-            <p className="mt-4 md:mt-6 text-[#785233] text-[10px] md:text-base leading-relaxed font-bold font-poppins">
+            <p className="mt-4 lg:mt-6 text-[#785233] text-[10px] lg:text-base leading-relaxed font-bold font-poppins">
               Himpunan Mahasiswa Teknologi Informasi <br />
               Universitas Udayana
             </p>
           </div>
         </div>
 
-        <div className="md:w-1/2 w-full relative">
+        <div className="lg:w-1/2 w-full relative">
           <div className="w-full h-full relative overflow-hidden">
             <img
               src="gedungHMTI.png"
-              alt="Gedung HMTI"
-              className="w-full h-full object-cover object-center"
+              alt="HMTI Udayana"
+              className="w-full h-full object-cover object-center hidden lg:flex"
             />
-            <div className="absolute inset-0 bg-gradient-to-l from-transparent to-white/10"></div>
+            <img
+              src="gedungHMTIMobile.png"
+              alt="HMTI Udayana"
+              className="w-full h-full object-cover object-center lg:hidden flex"
+            />
           </div>
         </div>
       </div>
