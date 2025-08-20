2419ae855cb991b0f6088daba5616d34cc51484f
PutuRifkiDy <puturifki56@gmail.com>
2025-08-20 14:09:39 +0800

    Update: img in loading animation

--- app/resources/js/Components/Loading.jsx
+++ app/resources/js/Components/Loading.jsx
@@ -7,7 +7,7 @@ const Loading = () => {
     <div className="fixed inset-0 flex items-center justify-center bg-white/80 dark:bg-[#1e1e1e]/70 z-50">
       <div className="flex flex-col items-center justify-center">
         <Lottie animationData={animationData} loop={true} className='w-60 h-60 animate-spin-slow relative'/>
-        <img src='assets/icon/logo_hmti.png' alt='Logo HMTI' className='w-16 h-16 absolute' />
+        <img src={`${window.location.origin}/assets/icon/logo_hmti.png`} alt='Logo HMTI' className='w-20 h-20 absolute' />
       </div>
     </div>
   );
